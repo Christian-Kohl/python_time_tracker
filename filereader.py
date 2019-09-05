9e8afe440956db78d7ba27b41f4b0792e6a71f81
Christian.Kohl <christianalbert.k@gmail.com>
2019-09-05 10:21:10 +0200

    Small changes made

--- filereader.py
+++ filereader.py
@@ -1,5 +1,11 @@
 # This class exists to get information from a file, and then also write it to any relevant files. The goal of this is to have a single file for every day, that has the date as a file name in the form of: 01/01/2000
 # Allowing for the
+import datetime as dt
+import json
+
+
+test_data_object = (application, time, )
+
 
 # Method, read xml file, creates object that embodies the file's information
 
