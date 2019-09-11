e0e7e023008bd6b53a81d193bd919c12d0d2fd86
Christian-Kohl <christianalbert.k@gmail.com>
2019-09-11 15:26:00 +0200

    Removed visualisation for later recreation, and actually added code from Kalle this time.

--- timetracker.py
+++ timetracker.py
@@ -1,6 +1,40 @@
 # Actual class to keep track of the time spent on the computer, it will take note of an individual application, or a specific website.
 
+
+def main():
+    return
+
 # Method to keep track of the time, and increment each few seconds, to see what exactly the computer is focussed on
+
+
+def get_active_window_raw():
+    '''
+    returns the details about the window not just the title
+    '''
+    root = subprocess.Popen(
+        ['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=subprocess.PIPE)
+    stdout, stderr = root.communicate()
+
+    m = re.search(b'^_NET_ACTIVE_WINDOW.* ([\w]+)$', stdout)
+    if m != None:
+        window_id = m.group(1)
+        window = subprocess.Popen(
+            ['xprop', '-id', window_id, 'WM_NAME'], stdout=subprocess.PIPE)
+        stdout, stderr = window.communicate()
+    else:
+        return None
+
+    match = re.match(b"WM_NAME\(\w+\) = (?P<name>.+)$", stdout)
+    if match != None:
+        ret = match.group("name").strip(b'"')
+        # print(type(ret))
+        '''
+        ret is str for python2
+        ret is bytes for python3 (- gives error while calling in other file)
+        be careful
+        '''
+        return ret
+    return None
 
 # Method to identify what application is being used by the user at this specific time
 
--- visualisation.py
+++
@@ -1,3 +0,0 @@
-# Methods to visualise the input data, this is solely to take the information, and use libraries such as seaborn, or matplotlib to make them readable.
-
-# This will be completed when the main functionality of the application is present, and functional.
