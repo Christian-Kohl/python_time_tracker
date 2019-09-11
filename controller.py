9a14f8212dbfc8127dcc43b398f5fb131c463243
Christian-Kohl <christianalbert.k@gmail.com>
2019-09-11 15:35:54 +0200

    Small beginning changes to controller, will be revisited when other sections are usable.

--- controller.py
+++ controller.py
@@ -1,3 +1,14 @@
-# Main class, to take in the user's commands, and the different flags/conditions. This will then use this to fetch the wanted information, or to keep track of time.
+from datetime import datetime
+import os
 
-# These methods will be added later, when the other classes are more established. It will however include the methods to implement click functionality.
+'''
+Main class, to take in the user's commands, and the different flags/conditions. This will then use this to fetch the wanted information, or to keep track of time.
+'''
+
+
+def tracker():
+    folder = "track_logs"
+    date = datetime.now().strftime('%d-%m-%Y')
+
+
+tracker()
