aa6e4a989f59f494b70ccebae11fad1bc14e9156
Christian-Kohl <christianalbert.k@gmail.com>
2021-03-09 14:14:10 +0000

    Changed dbManager file

--- dbManager.py
+++ dbManager.py
@@ -1,8 +1,22 @@
 import pymongo
+import datetime
+
+
+def insertEntry():
+    entryDict = {"time": datetime.datetime.now(),
+                 "program": "nudes",
+                 "window": "dudes"
+                 }
+    tracking.insert_one(entryDict)
+
 
 mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
 dblist = mongoClient.list_database_names()
-if "trackingDatabase" in dblist:
-    mongoDB = mongoClient["trackingDatabase"]
+mongoDB = 0
+mongoDB = mongoClient["trackingDatabase"]
 
 tracking = mongoDB["trackerEntry"]
+
+insertEntry()
+
+print(tracking.find_one())
