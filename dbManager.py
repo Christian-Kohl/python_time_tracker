cc0dff4b885c3c9a41651a3e083f54da130ad652
Christian-Kohl <christianalbert.k@gmail.com>
2021-03-11 18:28:44 +0000

    Added categories to the database

--- dbManager.py
+++ dbManager.py
@@ -10,12 +10,18 @@ def insertEntry(program, window):
     tracking.insert_one(entryDict)
 
 
+def insertCategory(category, item):
+    categoryDict = {"program": item,
+                    "category": category}
+    categories.insert_one(categoryDict)
+
+
 mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
 dblist = mongoClient.list_database_names()
 mongoDB = 0
 mongoDB = mongoClient["trackingDatabase"]
 
 tracking = mongoDB["trackerEntry"]
-
+categories = mongoDB["processCategories"]
 
 print(tracking.find_one())
